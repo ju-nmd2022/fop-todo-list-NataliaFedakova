17361819d353c01001de50e19a5bdc31ddd8a051
NataliaFedakova <117270929+NataliaFedakova@users.noreply.github.com>
2023-04-04 18:52:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,25 +2,38 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputContainer');
 
+
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value = ""; //clears the input
-    })
 
-  const spanElement = document.createElement("span");
-  spanElement.innerText = this.innerText;
-  toDoContainer.appendChild(spanElement);
-  
-  const button = document.createElement("button");
-  button.innerText = "Done";
-  button.onClick = removeElement;
-  toDoContainer.appendChild(button);
+    const button = document.createElement("button");
+    button.style = "background-color: #ffffff;";
+    button.innerText = "🗑️";
 
-  function removeElement(){
+    const buttonDone = document.createElement("button");
+    buttonDone.style = "background-color: #ffffff;";
+    buttonDone.innerText = "✔";
+
+    button.addEventListener("click", function removeElement(){
     const element = this.parentNode;
-    const paragraph = element.querySelector("span");
-    element.parentNode.removeChild(element);
-  }
+    toDoContainer.removeChild(paragraph);
+    toDoContainer.removeChild(button);
+    });
+
+    buttonDone.addEventListener("click", function doneElement(){
+      paragraph.style.textDecoration = "line-through";
+      const element = this.parentNode;
+      toDoContainer.removeChild(buttonDone);
+      });
+
+    paragraph.appendChild(button);
+    paragraph.appendChild(buttonDone);
+
+    toDoContainer.appendChild(paragraph);
+ })
+   
+  
